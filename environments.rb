ec9d046670a97c2f8492e68cd804ea7470f07f2b
aitbw <iangel.p93@gmail.com>
2015-09-16 23:11:47 -0430

    Added 'Bullet' gem to development env

--- environments.rb
+++ environments.rb
@@ -1,8 +1,15 @@
 configure :development do
   require 'better_errors'
+  require 'bullet'
   set :show_exceptions, true
   enable :logging
 
   use BetterErrors::Middleware
   BetterErrors.application_root = File.expand_path('..', __FILE__)
+
+  Bullet.enable = true
+  Bullet.alert = true
+  Bullet.bullet_logger = true
+  Bullet.console = true
+  use Bullet::Rack
 end
