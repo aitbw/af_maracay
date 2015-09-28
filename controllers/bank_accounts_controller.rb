e5ecb787155ff22c0a3d74cd65149e536592678d
aitbw <iangel.p93@gmail.com>
2015-09-27 23:51:39 -0430

    Validate bank account # uniqueness per bank

--- controllers/bank_accounts_controller.rb
+++ controllers/bank_accounts_controller.rb
@@ -1,7 +1,11 @@
 def asignar_cuenta
   query = Teacher.find(params[:id])
 
-  new_account = Account.new(idProfesor: params[:id], idBanco: params[:banco], numeroCuenta: params[:numero], tipoCuenta: params[:tipo])
+  if Account.where(idBanco: params[:banco], numeroCuenta: params[:numero]).present?
+    redirect "/dashboard/teachers/bank_accounts/#{params[:id]}/add", error: 'Esta cuenta ya existe.'
+  else
+    new_account = Account.new(idProfesor: params[:id], idBanco: params[:banco], numeroCuenta: params[:numero], tipoCuenta: params[:tipo])
+  end
 
   if new_account.save
     redirect "/dashboard/teachers/bank_accounts/#{params[:id]}", notice: "Cuenta asignada a #{query.nombreProfesor}"
