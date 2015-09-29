55024e6998d6382bade42d67239cf7e238de3722
Angel Perez <iangel.p93@gmail.com>
2015-09-29 08:59:56 -0430

    Added delete student feature

--- routes/students.rb
+++ routes/students.rb
@@ -13,3 +13,24 @@ end
 post '/dashboard/students/new_student' do
   nuevo_estudiante
 end
+
+get '/dashboard/students/delete/:id' do
+  begin
+    Estudiante.find(params[:id]).present?
+  rescue ActiveRecord::RecordNotFound
+    redirect '/dashboard/students', error: 'El estudiante no existe.'
+  else
+    @id_estudiante = params[:id]
+    @query = Estudiante.find(params[:id])
+    titulo('Eliminar estudiante — Panel de control')
+    erb :delete_student, layout: :'layouts/dashboard'
+  end
+end
+
+delete '/delete_student/:id' do
+  if Estudiante.destroy(params[:id])
+    redirect '/dashboard/students', notice: 'Estudiante eliminado.'
+  else
+    redirect "/dashboard/students/delete/#{params[:id]}", error: 'Ha ocurrido un error, intente nuevamente.'
+  end
+end
