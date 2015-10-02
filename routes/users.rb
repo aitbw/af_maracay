3b9281797fd7a8eb8706d7358d025694f1bdc64d
Angel Perez <iangel.p93@gmail.com>
2015-10-02 19:00:54 -0430

    Removed unnecessary redirect

--- routes/users.rb
+++ routes/users.rb
@@ -15,8 +15,6 @@ post '/dashboard/users/new_user' do
   if new_user.valid?
     secure_pass = BCrypt::Password.create(params[:usuario][:passwordUsuario])
     new_user.update(passwordUsuario: secure_pass)
-  else
-    redirect '/dashboard/users/new_user', flash[:error] = new_user.errors.full_messages
   end
 
   if new_user.save
