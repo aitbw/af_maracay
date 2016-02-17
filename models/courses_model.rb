840a408847a14104e0f060bffa2ad5a7d01c26cf
aitbw <iangel.p93@gmail.com>
2016-02-17 15:57:32 -0430

    Method delegation for Course model
    Modified RegEx for 'course_level', new 'course_code' format acc. to Alliance Francaise Maracay administration

--- models/courses_model.rb
+++ models/courses_model.rb
@@ -1,13 +1,22 @@
+require 'active_support/core_ext/module/delegation'
+
 # Model for 'courses' table
 class Course < ActiveRecord::Base
-  COURSE_CODE ||= /[ABC](1|2)[A-Z]{2}\d{2}/
-  COURSE_LEVEL ||= /[ABC](1|2)-(?:[01][0-9]|1[0-5])/
+  COURSE_CODE ||= /\d{4}-\w{4}\d{1}-\d{3}/
+  COURSE_LEVEL ||= /[ABC](?:1|2)-(?:0[1-9]|1[0-5])/
+
+  # Relations
   has_many :students, dependent: :destroy
+  has_many :grades, dependent: :destroy
   has_many :course_teachers
   has_many :teachers, through: :course_teachers
   belongs_to :course_types
   belongs_to :office
 
+  # Delegations
+  delegate :office_name, to: :office
+
+  # Validations
   validates :course_type_id, presence: true
   validates :office_id, presence: true
   validates :course_code, presence: true, uniqueness: true, format: { with: COURSE_CODE }
