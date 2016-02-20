bc57888d73caea4e0d56430aca3ddd1f73c1dae2
aitbw <iangel.p93@gmail.com>
2016-02-20 17:51:38 -0430

    Method delegation for Grade model

--- models/grades_model.rb
+++ models/grades_model.rb
@@ -1,8 +1,13 @@
+require 'active_support/core_ext/module/delegation'
+
 # Model for 'grades' table
 class Grade < ActiveRecord::Base
   # Relations
   belongs_to :student
   belongs_to :course
+
+  # Delegations
+  delegate :course_code, :course_level, to: :course
 
   # Validations
   validates :grade, presence: true, numericality: { only_integer: true }, length: { is: 2 }
