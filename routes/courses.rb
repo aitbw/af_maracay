a0a6e72e5f3b1e12f1893420558d6bc118fb5675
aitbw <iangel.p93@gmail.com>
2016-02-20 18:32:38 -0430

    Fixed: New/Edit Course views crashes

--- routes/courses.rb
+++ routes/courses.rb
@@ -18,7 +18,7 @@ end
 
 get '/dashboard/courses/new_course' do
   set_page_title('Crear nuevo curso')
-  @types = CourseType.select(:course_type_id, :course_type)
+  @types = CourseType.select(:course_type_id, :course_name)
   @offices = Office.all
   @js = ['moment.min.js', 'bootstrap-datetimepicker.min.js']
   erb :'new/new_course', layout: :'layouts/dashboard'
