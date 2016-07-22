3c5ba5cd706545245f22846741070bef19548b05
Angel Perez <iangel.p93@gmail.com>
2016-07-22 12:03:54 -0400

    Added instructions for CRON jobs when using MySQL/MariaDB

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,10 +1,12 @@
-# You need to execute 'whenever' on your shell to convert these events
+# You need to run 'whenever' on your shell to convert these events
 # to CRON syntax. If you want to run these jobs locally, you'll need to
-# type 'whenever --update-crontab' on your console.
+# type 'whenever --update-crontab' on your console. Be aware that the
+# SQL syntax used here is PG-specific. If you're using MySQL/MariaDB,
+# replace 'CURRENT_DATE' with 'CURDATE()'
 
 # If you're deploying to Heroku, please refer to Heroku Scheduler
 
-# This CRON Job was created to update the signups' statuses
+# This CRON job was created to update the signups' statuses
 # if they've expired, so the Alliance Francaise clerks know which
 # students need to renew their signups.
 every 1.day, at: '1:00 AM' do
@@ -12,7 +14,7 @@ every 1.day, at: '1:00 AM' do
   runner "ActiveRecord::Base.connection.execute(#{query})"
 end
 
-# This CRON Job was created to update the fees' statuses
+# This CRON job was created to update the fees' statuses
 # if they've expired, so the Alliance Francaise clerks know which
 # students need to renew their fees.
 every 1.day, at: '2:00 AM' do
