60a2d5c77f48c8e3c46c621db497bdcb06c5711c
Angel Perez <iangel.p93@gmail.com>
2016-09-19 23:06:38 -0400

    Code cleanup on Bank Accounts' module

--- controllers/bank_accounts_controller.rb
+++ controllers/bank_accounts_controller.rb
@@ -1,11 +1,29 @@
-def assign_account
+def new_bank_account
   new_account = BankAccount.new(params[:account])
 
   if new_account.save
-    flash[:notice] = 'Cuenta asignada exitosamente.'
-    redirect "/dashboard/teachers/#{params[:id]}/bank_accounts"
+    redirect "/dashboard/teachers/#{params[:id]}/bank_accounts", notice: 'Cuenta creada exitosamente.'
   else
     flash[:errors] = new_account.errors.full_messages
-    redirect "#{request.path_info}"
+    redirect(request.path_info.to_s)
+  end
+end
+
+def delete_bank_account
+  if BankAccount.destroy(params[:account])
+    flash[:notice] = 'Cuenta bancaria eliminada.'
+    redirect "/dashboard/teachers/#{params[:teacher]}/bank_accounts"
+  else
+    flash[:error] = 'Ha ocurrido un error, intente nuevamente.'
+    redirect(request.path_info.to_s)
+  end
+end
+
+def edit_bank_account(edit_account)
+  if edit_account.update(params[:form])
+    redirect "/dashboard/teachers/#{params[:teacher]}/bank_accounts", notice: 'Datos actualizados.'
+  else
+    flash[:errors] = edit_account.errors.full_messages
+    redirect(request.path_info.to_s)
   end
 end
---
+++ routes/bank_accounts.rb
@@ -0,0 +1,38 @@
+get '/dashboard/teachers/:id/bank_accounts' do
+  set_page_title('Cuentas bancarias')
+  @teacher = Teacher.find(params[:id])
+  @accounts = BankAccount.where(teacher_id: params[:id])
+  erb :bank_accounts, user_layout
+end
+
+get '/dashboard/teachers/:id/bank_accounts/new' do
+  set_page_title('Crear cuenta bancaria')
+  @banks = Bank.all
+  erb :'new/new_bank_account', user_layout
+end
+
+post '/dashboard/teachers/:id/bank_accounts/new' do
+  new_bank_account
+end
+
+get '/dashboard/teachers/:teacher/bank_accounts/:account/delete' do
+  set_page_title('Eliminar cuenta bancaria')
+  @account = BankAccount.find(params[:account])
+  erb :'delete/delete_bank_account', user_layout
+end
+
+delete '/dashboard/teachers/:teacher/bank_accounts/:account/delete' do
+  delete_bank_account
+end
+
+get '/dashboard/teachers/:teacher/bank_accounts/:account/edit' do
+  set_page_title('Editar cuenta bancaria')
+  @account = BankAccount.find(params[:account])
+  @banks = Bank.all
+  erb :'edit/edit_bank_account', user_layout
+end
+
+put '/dashboard/teachers/:teacher/bank_accounts/:account/edit' do
+  @edit_account = BankAccount.find(params[:account])
+  edit_bank_account(@edit_account)
+end
